7d189878114f8a25943759ac70d240a7be6eb830
Goncharov Vlad <holldike@gmail.com>
2021-12-07 14:18:07 +0200

    Fixed height and logo position

--- amadeo-app/components/header/Header.tsx
+++ amadeo-app/components/header/Header.tsx
@@ -77,15 +77,17 @@ const Header: React.FC = () => {
 
     return (
         <header>
-            <nav className="bg-white flex justify-between px-28 py-10">
-                <Image
-                    className=""
-                    src="/images/logo.svg"
-                    width={175}
-                    height={52}
-                    layout="fixed"
-                    alt=""
-                />
+            <nav className="bg-white flex items-center justify-between px-28 h-144">
+                <div className="relative -top-2">
+                    <Image
+                        src="/images/logo.svg"
+                        width={175}
+                        height={52}
+                        alt=""
+                    />
+                </div>
+
+
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     id="menu-button"
@@ -142,79 +144,79 @@ const Header: React.FC = () => {
                                 </li>
                             </>
                         ) : (
-                            <div className="hidden md:block">
-                                <div className="ml-4 flex items-center md:ml-6 md:mt-3">
-                                    <button
-                                        type="button"
-                                        className="bg-white p-1 rounded-full text-gray-400 hover:text-blue-200">
-                                        <span className="sr-only">View notifications</span>
-                                        <span className="relative inline-block">
-                                            <NoticeCounter delay={120000} />
-                                        </span>
-                                    </button>
+                                <div className="hidden md:block">
+                                    <div className="ml-4 flex items-center md:ml-6 md:mt-3">
+                                        <button
+                                            type="button"
+                                            className="bg-white p-1 rounded-full text-gray-400 hover:text-blue-200">
+                                            <span className="sr-only">View notifications</span>
+                                            <span className="relative inline-block">
+                                                <NoticeCounter delay={120000} />
+                                            </span>
+                                        </button>
 
-                                    {/* Profile dropdown */}
-                                    <Menu as="div" className="ml-5 relative -mt-2">
-                                        <div>
-                                            <Menu.Button
-                                                className="max-w-xs bg-gray-800 rounded-full flex
+                                        {/* Profile dropdown */}
+                                        <Menu as="div" className="ml-5 relative -mt-2">
+                                            <div>
+                                                <Menu.Button
+                                                    className="max-w-xs bg-gray-800 rounded-full flex
                                                 items-center text-sm focus:outline-none focus:ring-2
                                                 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                                                <span className="sr-only">Open user menu</span>
-                                                <Image
-                                                    src={userProfileImg}
-                                                    width={35}
-                                                    height={35}
-                                                    className="rounded-full"
-                                                    alt=""
-                                                />
-                                            </Menu.Button>
-                                        </div>
-                                        <Transition
-                                            as={Fragment}
-                                            enter="transition ease-out duration-100"
-                                            enterFrom="transform opacity-0 scale-95"
-                                            enterTo="transform opacity-100 scale-100"
-                                            leave="transition ease-in duration-75"
-                                            leaveFrom="transform opacity-100 scale-100"
-                                            leaveTo="transform opacity-0 scale-95">
-                                            <Menu.Items
-                                                className="origin-top-right absolute right-0 mt-2
+                                                    <span className="sr-only">Open user menu</span>
+                                                    <Image
+                                                        src={userProfileImg}
+                                                        width={35}
+                                                        height={35}
+                                                        className="rounded-full"
+                                                        alt=""
+                                                    />
+                                                </Menu.Button>
+                                            </div>
+                                            <Transition
+                                                as={Fragment}
+                                                enter="transition ease-out duration-100"
+                                                enterFrom="transform opacity-0 scale-95"
+                                                enterTo="transform opacity-100 scale-100"
+                                                leave="transition ease-in duration-75"
+                                                leaveFrom="transform opacity-100 scale-100"
+                                                leaveTo="transform opacity-0 scale-95">
+                                                <Menu.Items
+                                                    className="origin-top-right absolute right-0 mt-2
                                                 w-48 rounded-md shadow-lg py-1 bg-white ring-1
                                                 ring-white ring-opacity-5 focus:outline-none">
-                                                {userNavigation.map((item) => (
-                                                    <Menu.Item key={item.name}>
-                                                        {({ active }) => (
-                                                            <Link href={item.href}>
-                                                                <a
-                                                                    className={classNames(
-                                                                        active ? 'bg-gray-100' : '',
-                                                                        'block px-4 py-2 text-sm text-gray-700'
-                                                                    )}>
-                                                                    {item.name}
-                                                                </a>
-                                                            </Link>
-                                                        )}
-                                                    </Menu.Item>
-                                                ))}
-                                                <Menu.Item>
-                                                    <a
-                                                        href={`/api/auth/signout`}
-                                                        className="block px-4 py-2 text-sm text-gray-700"
-                                                        onClick={(e) => {
-                                                            e.preventDefault();
-                                                            window.localStorage.removeItem('user');
-                                                            signOut();
-                                                        }}>
-                                                        Sign out
+                                                    {userNavigation.map((item) => (
+                                                        <Menu.Item key={item.name}>
+                                                            {({ active }) => (
+                                                                <Link href={item.href}>
+                                                                    <a
+                                                                        className={classNames(
+                                                                            active ? 'bg-gray-100' : '',
+                                                                            'block px-4 py-2 text-sm text-gray-700'
+                                                                        )}>
+                                                                        {item.name}
+                                                                    </a>
+                                                                </Link>
+                                                            )}
+                                                        </Menu.Item>
+                                                    ))}
+                                                    <Menu.Item>
+                                                        <a
+                                                            href={`/api/auth/signout`}
+                                                            className="block px-4 py-2 text-sm text-gray-700"
+                                                            onClick={(e) => {
+                                                                e.preventDefault();
+                                                                window.localStorage.removeItem('user');
+                                                                signOut();
+                                                            }}>
+                                                            Sign out
                                                     </a>
-                                                </Menu.Item>
-                                            </Menu.Items>
-                                        </Transition>
-                                    </Menu>
+                                                    </Menu.Item>
+                                                </Menu.Items>
+                                            </Transition>
+                                        </Menu>
+                                    </div>
                                 </div>
-                            </div>
-                        )}
+                            )}
                     </ul>
                 </div>
             </nav>
