16df03a0a1694f9bee9f0c5f0008f9496a797d6a
Vlad <holldike@gmail.com>
2021-12-17 18:43:45 +0200

    Left side

--- amadeo-app/pages/auth/seller.tsx
+++ amadeo-app/pages/auth/seller.tsx
@@ -8,6 +8,7 @@ import { useTranslations } from 'next-intl';
 import ProviderBtns from '../../components/auth/ProviderBtns';
 import FullLayout from '../../components/layout/FullLayout';
 import Image from 'next/image';
+import InputTextDisabled from '../../components/_form/InputTextDisabled';
 
 interface Props {
     label: string | 'text';
@@ -73,9 +74,9 @@ function Seller({ providers, locale }: { providers: any; locale: string }) {
 
     return (
         <div className="flex justify-center">
-            <div className="mt-10 rounded-lg border shadow-xl flex justify-center w-[996px] bg-white">
-                <div className="p-20">
-                    <div className="flex mb-4">
+            <div className="px-24 py-14 mt-10 rounded-lg border shadow-xl flex justify-center w-[996px] bg-white">
+                <div className="pt-5 pr-20 border-r">
+                    <div className="flex mb-4 items-center">
                         <div className="mr-2.5 font-bold text-3xl line-height-105percent w-60">Your email has been verified!</div>
                         <Image
                             src="/images/tick.svg"
@@ -85,18 +86,12 @@ function Seller({ providers, locale }: { providers: any; locale: string }) {
                         />
                     </div>
 
-                    <div className="flex mb-4 border p-3 bg-gray-100 rounded-lg">
-                        <Image
-                            src="/images/input-email.svg"
-                            width="24"
-                            height="24"
-                            layout="fixed"
-                        />
+                    <InputTextDisabled icon="f-email" value="email@email.com" />
 
-                        <span className="ml-2.5 text-gray-180 font-bold text-sm">email@email.com</span>
-
+                    <div className="mb-3 mt-9 text-blue-350 text-sm">Almost there, please</div>
+                    <div className="w-60 text-5xl line-height-105percent font-bold">
+                        Complete your profile
                     </div>
-
                 </div>
                 <div className="w-6/12">
                     2
@@ -105,162 +100,6 @@ function Seller({ providers, locale }: { providers: any; locale: string }) {
         </div>
 
 
-    );
-
-    return (
-        <div className="flex justify-center min-h-screen">
-            <div className="container sm:mt-10 mt-10 my-auto max-w-md rounded-lg p-3 bg-white">
-                <div className="text-center my-6">
-                    <h1 className="text-3xl font-semibold text-gray-700">{t('Sign Up')}</h1>
-                </div>
-                <div className="m-6">
-                    {showAlert && (
-                        <div
-                            className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-10"
-                            role="alert">
-                            <span className="block sm:inline">{t(message as string)}</span>
-                            <button
-                                className="absolute top-0 bottom-0 right-0 px-4 py-3"
-                                onClick={() => {
-                                    setShowAlert(false);
-                                }}>
-                                <svg
-                                    className="fill-current h-6 w-6 text-red-500"
-                                    role="button"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20">
-                                    <title>Close</title>
-                                    <path
-                                        d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10
-                                        8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
-                    )}
-
-                    <form className="mb-4" onSubmit={handleSubmit(onSubmit)}>
-                        <input type="hidden" {...register('role_id')} defaultValue="2" />
-                        <InputText
-                            label={'Email Address'}
-                            type={'text'}
-                            name={'email'}
-                            register={register}
-                            errors={errors}
-                        />
-
-                        <InputText
-                            label={'Name'}
-                            type={'text'}
-                            name={'first_name'}
-                            register={register}
-                            errors={errors}
-                        />
-
-                        <InputText
-                            label={'Sername'}
-                            type={'text'}
-                            name={'last_name'}
-                            register={register}
-                            errors={errors}
-                        />
-
-                        <InputText
-                            label={'Company Name'}
-                            type={'text'}
-                            name={'company_name'}
-                            register={register}
-                            errors={errors}
-                        />
-
-                        <InputText
-                            label={'Identificator Number'}
-                            type={'text'}
-                            name={'identification_number'}
-                            register={register}
-                            errors={errors}
-                        />
-
-                        <InputText
-                            label={'Vat'}
-                            type={'text'}
-                            name={'vat'}
-                            register={register}
-                            errors={errors}
-                        />
-
-                        <InputText
-                            label={'Full Address'}
-                            type={'text'}
-                            name={'full_address'}
-                            register={register}
-                            errors={errors}
-                        />
-
-                        <InputText
-                            label={'Phone'}
-                            type={'text'}
-                            name={'phone'}
-                            register={register}
-                            errors={errors}
-                        />
-
-                        <InputText
-                            label={'Password'}
-                            type={'password'}
-                            name={'password'}
-                            register={register}
-                            errors={errors}
-                        />
-                        <InputText
-                            label={'Confirm Password'}
-                            type={'password'}
-                            name={'password_confirmation'}
-                            register={register}
-                            errors={errors}
-                        />
-
-                        <div className="mb-6">
-                            <input
-                                {...register('terms')}
-                                type="checkbox"
-                                name="terms"
-                                id="terms"
-                                placeholder=""
-                            />
-                            <label htmlFor="terms" className="ml-5">
-                                I agree with
-                                <Link href={'/pages/terms'}>
-                                    <a className="text-blue-500">Terms & Conditions</a>
-                                </Link>
-                            </label>
-                            <div className="error-el">{errors.terms?.message}</div>
-                        </div>
-
-                        <div className="mb-6">
-                            <button
-                                className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md
-                                    hover:bg-indigo-600
-                                    focus:outline-none duration-100 ease-in-out"
-                                disabled={formState.isSubmitting}>
-                                {formState.isSubmitting && (
-                                    <span className="spinner-border spinner-border-sm mr-1" />
-                                )}
-                                {t('Registration')}
-                            </button>
-                        </div>
-                    </form>
-                    <div className="flex flex-row justify-center mb-8">
-                        <span className="absolute bg-white px-4 text-gray-500">
-                            {t('or sign-up with')}
-                        </span>
-                        <div className="w-full bg-gray-200 mt-3 h-px" />
-                    </div>
-
-                    <ProviderBtns Providers={providers} locale={locale} />
-                </div>
-            </div>
-        </div>
     );
 }
 Seller.Layout = FullLayout;
