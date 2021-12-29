2b630afaa464606573ee4b5028e6b6bdd84ac350
Victory <malaya.kvl@gmail.com>
2021-12-29 15:12:26 +0200

    Add switcher (html only)

--- amadeo-app/components/lang/Switcher.tsx
+++ amadeo-app/components/lang/Switcher.tsx
@@ -15,24 +15,12 @@ export default function LangSwitcher() {
     return (
         <div className="relative">
             <div className="inline-block text-right dropdown">
-                <span className="rounded-md shadow-sm">
-                    <button
-                        className="inline-flex justify-center w-full px-4
-                            py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150
-                            ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500
-                            focus:outline-none focus:border-blue-300 focus:shadow-outline-blue
-                            active:bg-gray-50 active:text-gray-800">
-                        <span>{locale}</span>
-                        <svg className="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor">
-                            <path
-                                fillRule="evenodd"
-                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0
-                                    111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
-                    </button>
-                </span>
+                <button className="btn-langs">
+                    <span>{locale}</span>
+                </button>
+                <div className="lang-menu">
+                    <div className="corner" />
+                </div>
                 <div
                     className="opacity-0 invisible dropdown-menu
                         transition-all duration-300 transform
