803dac72ac3efd3e58364c70ee39788271d3d9cd
Vlad <holldike@gmail.com>
2022-01-20 12:27:18 +0200

    Validation

--- amadeo-app/pages/shipping/add-method.tsx
+++ amadeo-app/pages/shipping/add-method.tsx
@@ -1,33 +1,63 @@
+import { Formik } from "formik";
 import { useTranslations } from "next-intl";
 import { InputText } from '../../components/_form';
+import * as Yup from 'yup';
 
 export default function Shipping() {
     const t = useTranslations();
+    const submitSchema = Yup.object().shape({
+        name: Yup.string()
+            .min(3, t('Must be more characters'))
+            .required(t('Required field')),
+        logo: Yup.mixed()
+            .required(t('Required field'))
+    })
+
     return (
         <>
             <div className="mb-6 font-bold text-gray-350 text-lg border-gray-200">
                 {t('Add new shipping method')}
             </div>
-            <form className="w-1/2 p-4 mt-10 bg-gray-100 rounded-lg shadow-inner mb-6">
-                <label className="text-xs text-blue-350 font-bold">
-                    {t('Method name')}
-                    <InputText
-                        style="mt-4 w-52"
-                        icon={''}
-                        label={null}
-                        name={'method_name'}
-                        placeholder={'Name'}
-                        props={{ values: { method_name: '' }, errors: { method_name: '' } }}
-                        tips={null}
-                    />
-                </label>
-                <label className="block text-xs text-blue-350 font-bold">
-                    <div>{t('Method image')}</div>
-                    <input className="mt-4 w-52" type="file" />
-                </label>
 
-                <button className="mt-4 gradient-btn" type="submit">Create</button>
-            </form>
+            <Formik
+                enableReinitialize
+                initialValues={{}}
+                validationSchema={submitSchema}
+                onSubmit={(values) => {
+                    console.log(values)
+                }}
+                render={(props) => {
+                    return (
+                        <form onSubmit={props.handleSubmit} className="w-1/2 p-4 mt-10 bg-gray-100 rounded-lg shadow-inner mb-6">
+                            <label className="text-xs text-blue-350 font-bold">
+                                {t('Method name')}
+                                <InputText
+                                    style="mt-4 w-52"
+                                    icon={''}
+                                    label={null}
+                                    name={'name'}
+                                    placeholder={'Name'}
+                                    props={props}
+                                    tips={null}
+                                />
+                            </label>
+                            <label className="block text-xs text-blue-350 font-bold">
+                                <div>{t('Method image')}</div>
+                                <input className="mt-4 w-52" name="logo" type="file" onChange={(event) => {
+                                    props.setFieldValue("logo", event.currentTarget.files[0]);
+
+                                }} />
+                                {props.errors.logo ? (
+                                    <div className="error-el">{props.errors.logo}</div>
+                                ) : null}
+                            </label>
+
+                            <button className="mt-8 gradient-btn" type="submit">Create</button>
+                        </form>
+                    )
+                }}
+            />
+
         </>
     )
 }
