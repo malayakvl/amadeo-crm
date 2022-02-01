7ddfed505d29e76da290d1d3b905bb00cf8d2c73
Vlad <holldike@gmail.com>
2022-02-01 18:40:38 +0200

    Dropdown

--- amadeo-app/pages/shipping/list.tsx
+++ amadeo-app/pages/shipping/list.tsx
@@ -47,6 +47,7 @@ export default function List() {
     const countries = useSelector(countriesSelector);
     const user = useSelector(userSelector);
     const [threshold, setThreshold] = useState(false);
+    const [dropDowns, setDropDowns] = useState<Array<boolean>>([])
 
     useEffect(() => {
         if (!items) {
@@ -57,6 +58,8 @@ export default function List() {
             setupChecked.push({ id: item.id, checked: item.status });
         });
         dispatch(initIdsAction(setupChecked));
+        setDropDowns(items.map(() => false));
+
     }, [items]);
 
     useEffect(() => {
@@ -151,8 +154,21 @@ export default function List() {
                     sendCopyRequest={() => new Promise(() => null)}>
                     {items?.map((item: Shipping, index: number) => (
                         <tr key={item.id}>
-                            <td>
-                                <Image width="9" height="14" src={'/images/action-arrow.svg'} />
+                            <td onClick={() => {
+                                dropDowns[index] = !dropDowns[index]
+                                setDropDowns([...dropDowns])
+
+                            }}>
+                                {dropDowns[index]
+                                    ?
+                                    <>
+                                        <Image className='rotate-90 transform' width="12" height="14" src={`/images/action-arrow-orange.svg`} />
+                                        {/* <div className="border-l-2 h-4 relative left-[5px]" /> */}
+                                    </>
+                                    :
+                                    <Image width="12" height="14" src={`/images/action-arrow.svg`} />
+                                }
+
                             </td>
                             <td><div className="text-center">{index + 1}</div></td>
                             <td>{item.name}</td>
@@ -190,14 +206,19 @@ export default function List() {
                             </td>
                             {user.role_id !== 3 && (
                                 <td className="text-center">
-                                    {
-                                        item.countries.map((country) =>
+                                    {dropDowns[index]
+                                        ? item.countries.map((country) =>
                                             <div className="flex mb-1">
                                                 <Image width="34" height="24" src={'/images/en-flag.svg'} />
                                                 <div className="ml-auto">{country.price}</div>
                                             </div>
-                                        )
+                                        ) :
+                                        <div className="flex mb-1">
+                                            <Image width="34" height="24" src={'/images/en-flag.svg'} />
+                                            <div className="ml-auto">{item.countries[0].price}</div>
+                                        </div>
                                     }
+
                                     {/* {item.countries.map((country) => (
                                         <div
                                             key={country.id}
