91aca3bd82588469404ea67ec311ac9899ee80ec
Victory <malaya.kvl@gmail.com>
2022-02-01 19:44:53 +0200

    hide loader for unsuccess chatbot message

--- amadeo-app/redux/chatbot/actions.tsx
+++ amadeo-app/redux/chatbot/actions.tsx
@@ -36,7 +36,7 @@ export const submitFormAction: any = createAction(
                 })
                 .catch((e) => {
                     dispatch(setErrorToastAction(e.response.data.error));
-                    dispatch(showFormAction(false));
+                    dispatch(showLoaderAction(false));
                 });
         }
 );
