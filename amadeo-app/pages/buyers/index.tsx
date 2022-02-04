814bd5e3888da7cdbdab55a8db2acf8efb403fac
Vlad <holldike@gmail.com>
2022-02-04 18:29:44 +0200

    List template

--- amadeo-app/pages/buyers/index.tsx
+++ amadeo-app/pages/buyers/index.tsx
@@ -5,7 +5,56 @@ import Image from 'next/image';
 
 export default function Buyers() {
     const t = useTranslations();
-    const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    // const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const userProfileImg = 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80';
+
+    const items = [
+        {
+            username: 'username',
+            fullName: 'Some Full Name',
+            phone: '(406) 555-0120',
+            address: '3517 W.Gray St.Utica, Pennsylvania 57867',
+            orders: '999',
+            totalSpent: '9.845.25',
+            email: 'email@email.com'
+        },
+        {
+            username: 'username',
+            fullName: 'Some Full Name',
+            phone: '(406) 555-0120',
+            address: '3517 W.Gray St.Utica, Pennsylvania 57867',
+            orders: '999',
+            totalSpent: '9.845.25',
+            email: 'email@email.com'
+        },
+        {
+            username: 'username',
+            fullName: 'Some Full Name',
+            phone: '(406) 555-0120',
+            address: '3517 W.Gray St.Utica, Pennsylvania 57867',
+            orders: '999',
+            totalSpent: '9.845.25',
+            email: 'email@email.com'
+        },
+        {
+            username: 'username',
+            fullName: 'Some Full Name',
+            phone: '(406) 555-0120',
+            address: '3517 W.Gray St.Utica, Pennsylvania 57867',
+            orders: '999',
+            totalSpent: '9.845.25',
+            email: 'email@email.com'
+        },
+        {
+            username: 'username',
+            fullName: 'Some Full Name',
+            phone: '(406) 555-0120',
+            address: '3517 W.Gray St.Utica, Pennsylvania 57867',
+            orders: '999',
+            totalSpent: '9.845.25',
+            email: 'email@email.com'
+        },
+    ]
 
     return (
         <>
@@ -17,7 +66,7 @@ export default function Buyers() {
                     {t('Buyers section provides merchant information about his buyers')}
                 </div>
             </div>
-            <div className="ml-8 flex-1">
+            <div className="block-white-8 white-shadow-big">
                 <DataTable
                     hidePaginationBar
                     hideBulk
@@ -41,17 +90,32 @@ export default function Buyers() {
                                 </button>
                             </td>
                             <td className="text-center">
-                                <div className="text-center">{index + 1}</div>
+                                <div className="text-center text-orange-450 font-medium">{index + 1}</div>
                             </td>
-                            <td className="">text</td>
-                            <td className="p-0">
-                                <div className="text-center">text</div>
+                            <td className="">
+                                <div className="flex">
+                                    <div className="relative w-7 h-7">
+                                        <Image className="rounded-full" layout="fill" src={userProfileImg}/>
+                                    </div>
+                                    <div className="ml-2">
+                                        <div>{item.fullName}</div>
+                                        <div className="font-normal text-xs">{item.username}</div>
+                                        <div className="font-medium text-xs text-orange-450">{item.email}</div>
+                                    </div>
+                                </div>
                             </td>
-                            <td className="">text</td>
                             <td>
-                                <div className="text-center">text</div>
+                                <div className="text-center font-medium">{item.phone}</div>
                             </td>
-                            <td><div className="text-right">100 000 000</div></td>
+                            <td>
+                                <div className="font-medium">{item.address}</div>
+                            </td>
+                            <td>
+                                <div className="text-center text-orange-450">{item.orders}</div>
+                            </td>
+                            <td>
+                                <div className="text-right">{item.totalSpent}</div>
+                            </td>
                         </tr>
                     ))}
                 </DataTable>
