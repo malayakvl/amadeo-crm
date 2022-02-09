361d83e265c582e7eab2d1dd01e21e3d18197bc2
vladkrugov <andrei.p@solid-sl.com>
2022-02-09 13:03:35 +0200

    changed component PaymentMethods

--- amadeo-app/components/payments/PaymentMethods.tsx
+++ amadeo-app/components/payments/PaymentMethods.tsx
@@ -1,24 +1,66 @@
 import { useTranslations } from 'next-intl';
-import Image from 'next/image';
+import { useState, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { setSuccessToastAction } from '../../redux/layouts';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAmazon, faPaypal, faBitcoin } from '@fortawesome/free-brands-svg-icons';
 
 const PaymentMethods: React.FC = () => {
     const t = useTranslations();
+    const dispatch = useDispatch();
+
+    const [paymentMethods, setPaymentMethods] = useState([
+        {
+            id: 123,
+            name: 'Method_1_name',
+            icon: faAmazon,
+            active: false
+        },
+        {
+            id: 124,
+            name: 'Method_2_name',
+            icon: faPaypal,
+            active: false
+        },
+        {
+            id: 125,
+            name: 'Method_3_name',
+            icon: faBitcoin,
+            active: false
+        }
+    ]);
+
+    const handlerOnSave = useCallback(() => {
+        console.log('paymentMethods = ', paymentMethods);
+        dispatch(setSuccessToastAction(t('Payment methods saved')));
+    }, [paymentMethods]);
 
     return (
         <>
-            <div className="flex">
-                <div className="flex-1 text-center">
-                    <Image width="34" height="24" src={'/images/en-flag.svg'} />
+            {paymentMethods?.map((paymentMethod) => (
+                <div key={paymentMethod.id} className="flex space-x-2 items-center mb-2">
+                    <div className="flex-none w-7 h-auto">
+                        <FontAwesomeIcon icon={paymentMethod.icon} size="2x" />
+                    </div>
+                    <div className="h-auto flex-grow">{paymentMethod.name}</div>
+                    <div className="flex-none text-right">
+                        <label className="flex items-center cursor-pointer relative text-size">
+                            <input
+                                type="checkbox"
+                                className="sr-only"
+                                checked={paymentMethod.active}
+                                onChange={() => {
+                                    paymentMethod.active = !paymentMethod.active;
+                                    setPaymentMethods([...paymentMethods]);
+                                }}
+                            />
+                            <div className="toggle-bg bg-gray-200 border border-gray-200 rounded-full dark:bg-gray-700 dark:border-gray-600 w-11 h-6" />
+                        </label>
+                    </div>
                 </div>
-                <div className="flex-1">Method_name</div>
-                <div className="flex-1 text-right">
-                    <label className="flex items-center cursor-pointer relative">
-                        <input type="checkbox" className="sr-only" />
-                        <div className="toggle-bg bg-gray-200 border border-gray-200 rounded-full dark:bg-gray-700 dark:border-gray-600" />
-                    </label>
-                </div>
-            </div>
-            <button type="submit" className="w-full mt-8 gradient-btn">
+            ))}
+
+            <button onClick={handlerOnSave} className="w-full mt-8 gradient-btn">
                 {t('Save changes')}
             </button>
         </>
