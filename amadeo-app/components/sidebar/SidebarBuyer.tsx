86cb26b4bd8f619aec1f02f1de1fe1d2d219b580
Victory <malaya.kvl@gmail.com>
2022-04-05 14:20:17 +0300

    fix sidebar

--- amadeo-app/components/sidebar/SidebarBuyer.tsx
+++ amadeo-app/components/sidebar/SidebarBuyer.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 import Brand from './Brand';
 import Pages from './Pages';
+import { useDispatch, useSelector } from 'react-redux';
+import { isSidebarOpenSelector } from '../../redux/layouts/selectors';
+import { useWindowSize } from '../../hooks';
+import { sidebarCloseAction, toggleSidebarAction } from '../../redux/layouts';
 
 const SidebarBuyer: React.FC = () => {
     const t = useTranslations();
+    const isSidebarOpen = useSelector(isSidebarOpenSelector);
+    const dispatch = useDispatch();
+    const node = useRef<HTMLDivElement>(null);
+    const { isMobile } = useWindowSize();
+
+    const handleClick = (e: any) => {
+        if (node?.current?.contains(e.target) || node?.current === null) {
+            return;
+        }
+        if (isMobile) {
+            dispatch(sidebarCloseAction());
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClick);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClick);
+        };
+    }, []);
 
     return (
         <>
-            <div className="sidebar">
+            <div
+                className={`sidebar ${!isSidebarOpen && !isMobile ? 'sb-minimize' : ''} ${
+                    isSidebarOpen && isMobile ? 'mobile-open' : ''
+                }`}
+                ref={node}>
                 <div className="overflow-y-auto overflow-x-hidden flex flex-col justify-between flex-grow">
                     <ul>
                         <Brand />
@@ -32,6 +61,13 @@ const SidebarBuyer: React.FC = () => {
                     </ul>
                     <Pages />
                 </div>
+                <div
+                    className="sidebar-close-desctop"
+                    role="presentation"
+                    onClick={() => {
+                        dispatch(toggleSidebarAction());
+                    }}
+                />
             </div>
         </>
     );
