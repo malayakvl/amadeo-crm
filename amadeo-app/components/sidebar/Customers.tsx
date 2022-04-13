0bba43280afb88e7b1bc346aa61035127a060c0b
vladkrugov <andrei.p@solid-sl.com>
2022-04-13 16:06:45 +0300

    Added an arrow to the sidebar

--- amadeo-app/components/sidebar/Customers.tsx
+++ amadeo-app/components/sidebar/Customers.tsx
@@ -60,13 +60,27 @@ const SidebarCustomers: React.FC = () => {
                     </ul>
                     <Pages />
                 </div>
-                <div
-                    className="sidebar-close-desctop"
-                    role="presentation"
-                    onClick={() => {
-                        dispatch(toggleSidebarAction());
-                    }}
-                />
+
+                <button
+                    data-resize-button="true"
+                    aria-expanded="true"
+                    aria-label="Expand sidebar"
+                    type="button"
+                    className="sidebar-close-desktop"
+                    onClick={() => dispatch(toggleSidebarAction())}>
+                    <svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        role="presentation"
+                        className={isSidebarOpen ? '-rotate-180' : ''}>
+                        <path
+                            d="M10.294 9.698a.988.988 0 010-1.407 1.01 1.01 0 011.419 0l2.965 2.94a1.09 1.09 0 010 1.548l-2.955 2.93a1.01 1.01 0 01-1.42 0 .988.988 0 010-1.407l2.318-2.297-2.327-2.307z"
+                            fill="currentColor"
+                            fillRule="evenodd"
+                        />
+                    </svg>
+                </button>
             </div>
         </>
     );
